5ff89603b7e46b0aa2d9ea1ca1fd18034bcddc78
luismiguel13 <66281445+luismiguel13@users.noreply.github.com>
2022-09-06 10:36:22 -0500

    Update matriz_a.m

--- matriz_a.m
+++ matriz_a.m
@@ -1,12 +1,6 @@
-function fam = matriz_a(nfotos, nfamosos, rows , cols, nfam, original)
-    %Transformar para recibir las 5 fotos de diferente tamaño, hacer el resize y generar la matriz A
-    fam = zeros(nfotos,120*80);
+function fam = matriz_a(nfotos, m, n, images)
+    fam = zeros(nfotos,m*n);
     for i = 1:nfotos
-        min_col = (cols/nfotos)*(i-1)+1;
-        max_col = (cols/nfotos)*i;
-        min_row = (rows/nfamosos)*(nfam-1)+1;
-        max_row = (rows/nfamosos)*nfam;
-        fam(i,:) = reshape(transpose(original(min_row:max_row,min_col:max_col)),[1,120*80]);
-        %imshow(original(min_row:max_row,min_col:max_col))
+        fam(i,:) = reshape(transpose(images(:,:,i)),[1,m*n]);
     end
 end
